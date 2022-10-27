6b395d2a44971073825bf84f73fe77f86ac6f512
jsantos4 <johnlsantos19@gmail.com>
2022-10-27 18:07:27 -0400

    Remembering how header files work

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,93 +1,90 @@
 #include <Map.hpp>
 #include <memory.h>
 
-class Map {
+int width, height;
+unsigned char** cells;
 
-	int width, height;
-	unsigned char** cells;
-
-	Map::Map(int w, int h) {
-		width = w;
-		height = h;
-		//Gonna get kinda fucked up here, cells is a new array "width" long and 0-set each array within cells[x]
-		cells = new unsigned char*[width];
-		for (int x = 0; x < width; x++) {
-			cells[x] = new unsigned char;
-			memset(cells[x], 0, height);
-		}
+Map::Map(int w, int h) {
+	width = w;
+	height = h;
+	//Gonna get kinda fucked up here, cells is a new array "width" long and 0-set each array within cells[x]
+	cells = new unsigned char*[width];
+	for (int x = 0; x < width; x++) {
+		cells[x] = new unsigned char;
+		memset(cells[x], 0, height);
 	}
+}
 
-	Map::~Map() {
-		for (int x = 0; x < width; x++) {
-			delete[] cells[x];
-		}
-		delete[] cells;
+Map::~Map() {
+	for (int x = 0; x < width; x++) {
+		delete[] cells[x];
 	}
+	delete[] cells;
+}
 
-	void Map::birthCell(int x, int y) {
+void Map::birthCell(int x, int y) {
 
-		cells[x][y] |= 1;
+	cells[x][y] |= 1;
 
-		//Increment all neighboring cells neighbor counter if neighbor exists
-			//Check up-center and down-center for boundaries
+	//Increment all neighboring cells neighbor counter if neighbor exists
+		//Check up-center and down-center for boundaries
+	if (y < height)
+		cells[x][y + 1] += 2;
+	if (y > 0)
+		cells[x][y - 1] += 2;
+		
+		//Check  left and right columns for boundaries
+	if (x < width){
+		cells[x + 1][y] += 2;
+		
 		if (y < height)
-			cells[x][y + 1] += 2;
+			cells[x + 1][y + 1] += 2;
 		if (y > 0)
-			cells[x][y - 1] += 2;
-			
-			//Check  left and right columns for boundaries
-		if (x < width){
-			cells[x + 1][y] += 2;
-			
-			if (y < height)
-				cells[x + 1][y + 1] += 2;
-			if (y > 0)
-				cells[x + 1][y - 1] += 2;
-		}
-
-		if (x > 0) {
-			cells[x - 1][y] += 2;
-
-			if (y < height)
-				cells[x - 1][y + 1] += 2;
-			if (y > 0)
-				cells[x - 1][y - 1] += 2;
-		}
+			cells[x + 1][y - 1] += 2;
 	}
 
+	if (x > 0) {
+		cells[x - 1][y] += 2;
 
-	void Map::killCell(int x, int y) {
-
-		cells[x][y] &= ~1;
-
-		//Decriment all neighboring cells neighbor counter if neighbor exists
-			//Check up-center and down-center for boundaries
 		if (y < height)
-			cells[x][y + 1] -= 2;
+			cells[x - 1][y + 1] += 2;
 		if (y > 0)
-			cells[x][y - 1] -= 2;
-			
-			//Check  left and right columns for boundaries
-		if (x < width){
-			cells[x + 1][y] -= 2;
-			
-			if (y < height)
-				cells[x + 1][y + 1] -= 2;
-			if (y > 0)
-				cells[x + 1][y - 1] -= 2;
-		}
+			cells[x - 1][y - 1] += 2;
+	}
+}
 
-		if (x > 0) {
-			cells[x - 1][y] -= 2;
 
-			if (y < height)
-				cells[x - 1][y + 1] -= 2;
-			if (y > 0)
-				cells[x - 1][y - 1] -= 2;
-		}
+void Map::killCell(int x, int y) {
+
+	cells[x][y] &= ~1;
+
+	//Decriment all neighboring cells neighbor counter if neighbor exists
+		//Check up-center and down-center for boundaries
+	if (y < height)
+		cells[x][y + 1] -= 2;
+	if (y > 0)
+		cells[x][y - 1] -= 2;
+		
+		//Check  left and right columns for boundaries
+	if (x < width){
+		cells[x + 1][y] -= 2;
+		
+		if (y < height)
+			cells[x + 1][y + 1] -= 2;
+		if (y > 0)
+			cells[x + 1][y - 1] -= 2;
 	}
 
-	unsigned char Map::getCell(int x, int y) {
-		return cells[x][y];
+	if (x > 0) {
+		cells[x - 1][y] -= 2;
+
+		if (y < height)
+			cells[x - 1][y + 1] -= 2;
+		if (y > 0)
+			cells[x - 1][y - 1] -= 2;
 	}
-};
+}
+
+unsigned char Map::getCell(int x, int y) {
+	return cells[x][y];
+}
